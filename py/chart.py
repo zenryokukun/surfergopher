88db4f6dc4512e25e2329c8ba849d7c12324006d
crypto <bathingasian001@gmail.com>
2022-09-08 23:29:46 +0900

    fixed graph

--- py/chart.py
+++ py/chart.py
@@ -5,7 +5,6 @@ Generates tweet image from
 import json
 import datetime
 from pathlib import Path
-from tokenize import Number
 import matplotlib.pyplot as plt
 
 
@@ -114,6 +113,11 @@ def slice_after(obj, after: int):
     return ret
 
 
+def slice_backwards(obj, after: int):
+    ret = {k: v[-after:] for k, v in obj.items()}
+    return ret
+
+
 def first_matched(targ, arr):
     for i, v in enumerate(arr):
         if v == targ:
@@ -135,30 +139,40 @@ def toLocal(tstamp: list[int]) -> list[int]:
 
 if __name__ == "__main__":
 
-    dlen = 180
+    dlen = 400  # 表示するデータ数
 
     with open(BDATA_PATH) as f:
+        # dlen分のデータになるよう、古いデータは落とす
         bdata = json.load(f)
-        bdata = slice_after(bdata, dlen)
-
-    start_v = bdata["X"][0]
+        bdata = slice_backwards(bdata, dlen)
 
     with open(CDATA_PATH) as f:
+        # dlen分のデータになるよう、古いデータは落とす
         cdata = json.load(f)
-        # cdata["OpenTime"] = toLocal(cdata["OpenTime"])
-        i = first_matched(start_v, cdata["OpenTime"])
-        cdata = slice_after(cdata, i)
+        cdata = slice_backwards(cdata, dlen)
+
+    # ろうそくデータの一番はじめの時間
+    start_v = cdata["OpenTime"][0]
 
     with open(PDATA_PATH) as f:
+        '''
+        取引データは取引のあった時間のみ出力される。bdataのように毎フレーム出力はされないため、
+        ろうそくデータの最初の時間より後のデータのみを残す。
+        '''
         pdata = json.load(f)
         i = nearest(start_v, pdata["X"])
         pdata = slice_after(pdata, i)
 
+    # 稼働初めでbdataがcdataより短い場合等を想定。今は該当しないと思う。
     # candlesのほうが長い場合、長さをblenに合わせる
     blen = len(bdata["X"])
     clen = len(cdata["Open"])
     if clen > blen:
         for key in cdata.keys():
             cdata[key] = cdata[key][-blen:]
+    # candlesのほうが短い場合、長さをclenに合わせる
+    elif clen < blen:
+        for key in bdata.keys():
+            bdata[key] = bdata[key][-clen:]
 
     chart(cdata, pdata, bdata, OUT_PATH)
